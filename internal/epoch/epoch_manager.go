192233d114cc4e730ba6f3f5804c9c5c143bcf07
Lyndon-Li <lyonghui@vmware.com>
2024-03-22 16:29:25 +0800

    add allow-index-write-on-load to open options
    Signed-off-by: Lyndon-Li <lyonghui@vmware.com>

--- internal/epoch/epoch_manager.go
+++ internal/epoch/epoch_manager.go
@@ -1128,21 +1128,25 @@ func rangeCheckpointBlobPrefix(epoch1, epoch2 int) blob.ID {
 	return blob.ID(fmt.Sprintf("%v%v_%v_", RangeCheckpointIndexBlobPrefix, epoch1, epoch2))
 }
 
-func allowWritesOnIndexLoad() bool {
+func allowWritesOnIndexLoad(fromParam bool) bool {
+	if fromParam {
+		return true
+	}
+
 	v := strings.ToLower(os.Getenv("KOPIA_ALLOW_WRITE_ON_INDEX_LOAD"))
 
 	return v == "true" || v == "1"
 }
 
 // NewManager creates new epoch manager.
-func NewManager(st blob.Storage, paramProvider ParametersProvider, compactor CompactionFunc, log logging.Logger, timeNow func() time.Time) *Manager {
+func NewManager(st blob.Storage, paramProvider ParametersProvider, compactor CompactionFunc, log logging.Logger, timeNow func() time.Time, optAllowWriteOnIndexLoad bool) *Manager {
 	return &Manager{
 		st:                            st,
 		log:                           log,
 		compact:                       compactor,
 		timeFunc:                      timeNow,
 		paramProvider:                 paramProvider,
-		allowCleanupWritesOnIndexLoad: allowWritesOnIndexLoad(),
+		allowCleanupWritesOnIndexLoad: allowWritesOnIndexLoad(optAllowWriteOnIndexLoad),
 		getCompleteIndexSetTooSlow:    new(int32),
 		committedStateRefreshTooSlow:  new(int32),
 		writeIndexTooSlow:             new(int32),
